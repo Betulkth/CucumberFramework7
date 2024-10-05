4c30ea37a61c4d19ddd61d400e2d78888e1db64b
Betul Taylor Hayden <betulkaratastaylorhayden@gmail.com>
2024-10-05 19:15:54 -0400

    Cucumber Framework7

---
+++ src/test/java/com/neotech/steps/CreateNewDisciplinaryActionsteps.java
@@ -0,0 +1,54 @@
+package com.neotech.steps;
+
+public class CreateNewDisciplinaryActionsteps {
+
+	public static void firstStep() {
+		
+//		 * artiya tikla add new disciplinary
+//		 * Add Disciplinary Case modal displayed
+//		 * emp nam and case name description send text click save
+		
+	}
+	
+	public static void secondStep() {
+//		 * click take Disciplinary action
+//		 * wait for Disciplinary Actions modal
+		
+	}
+	
+	public void steps() {
+		
+		/*
+		 // firstStep(); metodu cagir
+		 *
+		 * 1. assertion !!! take Disciplinary action button verification (assertion) ayni zamanda assertion ile  1. testcase tamamlandi
+		 * 
+		 * secondStep() metod cagir
+
+		 * 
+		 * 2. assertion !! take the list of 8 options and use assertion to verify that all of them are displayed.
+		 * 
+		 * 
+		 * for(8 options adhanced for loop)
+		 * {    
+		 *
+		 * 		select option and click select
+		 *
+		 * 		3. assertion !! Add Action is displayed. waitforvisibility 
+		 * 
+		 *		tum elementleri kontrol et ve click save “Successfully Added mesajini gor
+		 *
+		 *		click disciplinary cases
+		 *	
+		 *		firstStep(); metodu cagir
+		 *		secondStep();metodu cagir
+		 *}
+		 *----------------------------------------------------------------------------
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+	}
+}
